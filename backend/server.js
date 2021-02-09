b047e1d9a33d267fd6f7c98856ac02729a57489c
SChanakya <chanakyachana@gmail.com>
2021-02-09 20:52:50 +0530

    react hook is created

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,15 @@ import express from 'express';
 import data from './data.js';
 
 const app = express();
-app.get('/', (req, res)=> {
-    res.send('Servers is OK');
-});
 
 app.get('/api/products', (req, res) => {
-    res.send(data.products);
-})
+  res.send(data.products);
+});
 
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
 const port = process.env.PORT || 5000;
-app.listen(5000, () => {
-    console.log(`Serve at http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Server at http://localhost:${port}`);
 });
