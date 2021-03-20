601b8d60f853018bf4d207de2a5a0d8594bc8c8f
SChanakya <chanakyachana@gmail.com>
2021-03-20 12:25:25 +0530

    registerScreen and redirecting

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -36,4 +36,21 @@ userRouter.post('/signin',expressAsyncHandler(async(req, res) =>{
 
 );
 
+userRouter.post('/register', expressAsyncHandler(async(req, res) => {
+    const user = new User({name: req.body.name, email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 8),
+    });
+
+    const createdUsers = await user.save();
+    res.send({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(createdUsers),
+
+    })
+}))
+
+
 export default userRouter;
